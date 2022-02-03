a1e3db2f1a32fe33715526471cc19a967aa41587
shaky-sahil <sahilpk81@gmail.com>
2022-02-03 08:03:45 +0530

    one to one rln

--- db/migrate/20220203020733_create_events.rb
+++ db/migrate/20220203020733_create_events.rb
@@ -1,7 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
-
+      t.string :name
+      t.date :date
+      t.string :location
+      t.belongs_to :user
       t.timestamps
     end
   end
